5c94b4bd1d010b326f26fbe330847c1bed4a795f
SanderJanssen91 <janssen.sander@live.nl>
2017-11-07 17:15:15 +0100

    Start weather app (retrieving lat and long from googleapi dynamically)

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,30 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+  console.log(argv.address);
+  var encodedAddress = encodeURIComponent(argv.address);
+  var stdUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=104%20dennenstraat%20nijmegen',
+  url: stdUrl+encodedAddress,
   json: true
 }, (error, response, body) => {
-  console.log(body);
+    var lat = body.results[0].geometry.location.lat;
+    var lng = body.results[0].geometry.location.lng;
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${lat}`);
+    console.log(`Long: ${lng}`);
 });
